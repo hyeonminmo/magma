22309a1ae659867e45491b48e710bcb7667c915b
Yunho Kim <yunhokim@hanyang.ac.kr>
2025-02-12 13:58:00 +0900

    Use the recent version of CMake

--- targets/poppler/preinstall.sh
+++ targets/poppler/preinstall.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+#echo "deb-src http://archive.ubuntu.com/ubuntu/ xenial main restricted" | sudo tee -a /etc/apt/sources.list && \
 apt-get update && \
-    apt-get install -y git make autoconf automake libtool pkg-config cmake \
+    apt-get install -y git make autoconf automake libtool pkg-config \
         zlib1g-dev libjpeg-dev libopenjp2-7-dev libpng-dev libcairo2-dev \
-        libtiff-dev liblcms2-dev libboost-dev
+        libtiff-dev liblcms2-dev libboost-dev wget
+
+wget https://github.com/Kitware/CMake/releases/download/v3.31.5/cmake-3.31.5-linux-x86_64.tar.gz
+tar zxf cmake-3.31.5-linux-x86_64.tar.gz
+cd cmake-3.31.5-linux-x86_64
+cp * -fr -t /usr
